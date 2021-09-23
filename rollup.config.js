e702fb158fb2e011ecaa1920f9d0c2a57fbda245
Benjamin <eunvanz@naver.com>
2021-09-23 21:53:26 +0900

    feat: release 0.1.0

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import pkg from "./package.json";
+import babel from "rollup-plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx", ".scss"];
+
+process.env.BABEL_ENV = "production";
+
+export default [
+  {
+    input: pkg.source,
+    output: [
+      {
+        file: pkg.module,
+        sourcemap: true,
+        format: "es",
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve({ extensions }),
+      commonjs({
+        include: /node_modules/,
+      }),
+      postcss({
+        extract: true,
+        modules: true,
+        sourceMap: true,
+        use: ["sass"],
+      }),
+      babel({ extensions, include: ["src/**/*"], runtimeHelpers: true }),
+      terser(),
+    ],
+    external: ["react", "react-dom"],
+  },
+];
