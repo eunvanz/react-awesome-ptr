64150a1197b8c0b2879aa6fd7344b89af7174c04
Benjamin <eunvanz@naver.com>
2021-12-12 14:58:15 +0900

    feat: export CupertinoSpinner in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import PullToRefresh from "./PullToRefresh";
+export { default as CupertinoSpinner } from "./CupertinoSpinner";
 
 export type { PullToRefreshProps } from "./PullToRefresh";
 export type { PullToRefreshState } from "./CommonPullToRefresh";
+export type { CupertinoSpinnerProps } from "./CupertinoSpinner";
 
 export default PullToRefresh;
